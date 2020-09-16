71125b13b7e599d87cad8faa3daca392aa9dae97
frogurt-p <71208704+frogurt-p@users.noreply.github.com>
2020-09-16 15:07:08 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,6 +96,76 @@ ijin lewat ndan
            }
             
        }
+ //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+     if (command.toLowerCase() === 'votenick' && args){
+
+            let isGoodToGo = false;
+            let filteredIndex;
+            const usermention = message.mentions.members.first();
+            
+            if ( (args.length >=2) && usermention ){
+                isGoodToGo = true
+            filteredIndex = args.filter((value, index) =>{
+                return index >= 1
+            }) 
+            } else return message.channel.send('no nickname');
+
+
+
+     if(isGoodToGo){
+             
+             let firstMsg = await message.channel.send(`ubah nama ${usermention} ke ${filteredIndex.join(' ')} (15 detik)`)
+             let poolPositive = 0;
+             let poolNegative = 0;
+            
+             const filterPositive = (reaction , user) =>{ return reaction.emoji.name === '✅' && user.id }
+            const filterNegative = (reaction, user) => { return reaction.emoji.name === '❌' && user.id }
+            
+            const collectorPositive = message.createReactionCollector(filterPositive , { time : 15000});
+            collectorPositive.options.dispose = true
+            collectorPositive.on('collect', (r , user) => {
+                poolPositive++;
+                console.log(`collected positive pool from ${user.tag} and ${poolPositive}`)});
+            collectorPositive.on('remove', (r , user)=>{
+                poolPositive--;
+                console.log(`removed positive pool from ${user.tag} and ${poolPositive}`)
+            });
+            collectorPositive.on('end', () => console.log(`${poolPositive} positive items have been collected`) );
+
+            const collectorNegative = message.createReactionCollector(filterNegative , { time : 15000});
+            collectorNegative.options.dispose = true
+            collectorNegative.on('collect', (r , user) => {
+                poolNegative++;
+                console.log(`collected negative pool from ${user.tag} and ${poolNegative}`)});
+            collectorNegative.on('remove', (r , user)=>{
+                poolNegative--;
+                console.log(`removed positive pool from ${user.tag} and ${poolNegative}`)
+            });
+            collectorNegative.on('end', () => console.log(`${poolNegative} negative items have been collected`)  );
+            
+            
+            message.react('✅');
+            message.react('❌');
+
+
+        
+            setTimeout(() =>{
+                
+                if (poolPositive > poolNegative){
+                
+                usermention.setNickname(filteredIndex.join(' '));
+
+                firstMsg.edit(`nickname ${usermention} dirubah(${poolPositive} upvotes and ${poolNegative} downvotes)`);
+                }
+                else firstMsg.edit(`nickname ${usermention} tidak dirubah(${poolPositive} upvotes and ${poolNegative} downvotes)`);
+              
+              
+            }, 15000);
+
+     }
+
+         }
+ 
  
  //----------------------------------------------------------------------------------------------------------------------------------------------------------------
         if (command.toLowerCase() === 'usir' && message.member.voice.channel){
